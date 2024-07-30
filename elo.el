c87e9987020acc32d69d908dff2afb54e1e448d7
Sergey Lilo <svlilo@yandex.ru>
2024-07-30 13:02:40 +0600

    fights plist

--- elo.el
+++ elo.el
@@ -42,19 +42,19 @@
   "Current entry as alist."
   (interactive)
   (let* ((pom (point))
-         (id (org-entry-get pom "ID"))
          (elo (org-entry-get pom "ELO"))
          (title (org-entry-get pom "ITEM"))
-         ;; TODO: plist (p1-id num-p1-p2-games ...)
-         (fights (org-entry-get-multivalued-property pom "ELO_FIGHTS"))
-         (num-fights (if (consp fights)
-                         (length fights)
-                       0)))
-    `((id . ,id)
-      (elo . ,(if (stringp elo) (string-to-number elo) org-elo-starting-elo))
+         (plist-fights
+          (org-entry-get-multivalued-property pom "ELO_FIGHTS"))
+         (plist-numfights ;; sum values in plist
+          (cl-loop
+           for n from 2 to (length plist-fights) by 2
+           sum (string-to-number (nth (1- n) plist-fights)))))
+    `((elo . ,(if (stringp elo) (string-to-number elo) org-elo-starting-elo))
       (title . ,title)
-      (fights . ,fights)
-      (num-fights . ,num-fights))))
+      (fights . ,plist-fights)
+      (num-fights . ,plist-numfights))))
+
 
 (defun org-elo-compare-tabulated (item1 item2)
   "Compare tabulated items by rating"
@@ -146,42 +146,53 @@
 (defun org-elo-fight-update (p1-winner-p)
   "Update records for current pair.
 Set elo."
-  (let* ((p1 org-elo-p1)
-         (p2 org-elo-p2)
-         (p1-id (let-alist p1 .id))
-         (p2-id (let-alist p2 .id))
+  (let* ((p1 (copy-alist org-elo-p1))
+         (p2 (copy-alist org-elo-p2))
          (p1-title (let-alist p1 .title))
          (p2-title (let-alist p2 .title))
          (p1-fights (let-alist p1 .fights))
          (p2-fights (let-alist p2 .fights))
+         (p1-vs-p2-num
+          (1+ (string-to-number
+           (or
+            (plist-get p2-fights p1-title #'equal)
+            "0"))))
+         (p2-vs-p1-num
+          (1+ (string-to-number
+           (or
+            (plist-get p1-fights p2-title #'equal)
+            "0"))))
          (p1-elo (or (let-alist p1 .elo) org-elo-starting-elo))
          (p2-elo (or (let-alist p2 .elo) org-elo-starting-elo))
-         (new-elos (if
-                       p1-winner-p
-                       (org-elo-compute-elo p1-elo p2-elo)
-                     (org-elo-compute-elo p2-elo p1-elo)))
+         (new-elos
+          (if
+              p1-winner-p
+              (org-elo-compute-elo p1-elo p2-elo)
+            (org-elo-compute-elo p2-elo p1-elo)))
          (p1-new-elo (if p1-winner-p (car new-elos) (cdr new-elos)))
-         (p2-new-elo (if p1-winner-p (cdr new-elos) (car new-elos))))
+         (p2-new-elo (if p1-winner-p (cdr new-elos) (car new-elos)))
+         (p1-fights (plist-put p1-fights p2-title
+                               (number-to-string p2-vs-p1-num) #'equal))
+         (p2-fights (plist-put p2-fights p1-title
+                               (number-to-string p1-vs-p2-num) #'equal)))
     (save-excursion
       (with-current-buffer (find-file-noselect org-elo-file)
-        (let* ((p1pom (org-id-find p1-id :marker))
-               (p2pom (org-id-find p2-id :marker)))
+        (let* ((p1pom (org-elo-get-by-title p1-title))
+               (p2pom (org-elo-get-by-title p2-title)))
           (org-entry-put p1pom "ELO" (number-to-string p1-new-elo))
-          (org-entry-put p1pom "ELO_FIGHTS"
-                         (if p1-fights
-                             (mapconcat
-                              'identity
-                              (append p1-fights (list p2-id)) " ")
-                           p2-id))
           (org-entry-put p2pom "ELO" (number-to-string p2-new-elo))
-          (org-entry-put p2pom "ELO_FIGHTS"
-                         (if p2-fights
-                             (mapconcat
-                              'identity
-                              (append p2-fights (list p1-id)) " ")
-                           p1-id))
-          (save-buffer)))))
-  (org-elo-fight-revert))
+          (org-entry-delete p1pom "ELO_FIGHTS")
+          (org-entry-delete p2pom "ELO_FIGHTS")
+          (apply `(org-entry-put-multivalued-property
+                   ,p1pom
+                   "ELO_FIGHTS"
+                   ,@p1-fights))
+          (apply `(org-entry-put-multivalued-property
+                   ,p2pom
+                   "ELO_FIGHTS"
+                   ,@p2-fights))
+          (save-buffer))))
+  (org-elo-fight-revert)))
 
 (define-derived-mode org-elo-fight-mode special-mode "org-elo-fight-mode"
   "Fight!"
