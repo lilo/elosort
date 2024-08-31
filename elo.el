77fb9c6a7f10c845ec3efee41ff543729633fadd
Sergey Lilo <svlilo@yandex.ru>
2024-09-01 02:18:56 +0600

    rename project to `elosort'. Replace the prefix.

--- elo.el
+++ elo.el
@@ -2,18 +2,18 @@
 ;;; Commentary:
 ;;; Code:
 
-(defcustom org-elo-k 20
+(defcustom elosort-k 20
   "The K."
   :group 'elo)
 
-(defcustom org-elo-starting-elo 1200
+(defcustom elosort-starting-elo 1200
   "Starting elo."
   :group 'elo)
 
-(defvar org-elo-file nil
+(defvar elosort-file nil
   "The DB file.")
 
-(defun org-elo-get-by-title (title)
+(defun elosort-get-by-title (title)
   "Return POM of the heading with matching TITLE."
   (cl-dolist (pt (org-map-entries #'point))
     (when
@@ -27,16 +27,16 @@
           #'string-equal))
       (cl-return pt))))
 
-(defun org-elo-compute-elo (winner loser)
+(defun elosort-compute-elo (winner loser)
   "Compute new elo for WINNER and LOSER using K."
-  (let* ((k org-elo-k)
+  (let* ((k elosort-k)
          (p1 (/ 1.0 (+ 1.0 (expt 10 (/ (- loser winner) 400.0)))))
          (w (+ winner (* k (- 1 p1))))
          (p2 (/ 1.0 (+ 1.0 (expt 10 (/ (- winner loser) 400.0)))))
          (l (+ loser (* k (- 0 p2)))))
     (cons (round w) (round l))))
 
-(defun org-elo-get-alist ()
+(defun elosort-get-alist ()
   "Current entry as alist."
   (interactive)
   (let* ((pom (point))
@@ -48,13 +48,13 @@
           (cl-loop
            for n from 1 to (length plist-fights) by 2
            sum (string-to-number (nth n plist-fights)))))
-    `((elo . ,(if (stringp elo) (string-to-number elo) org-elo-starting-elo))
+    `((elo . ,(if (stringp elo) (string-to-number elo) elosort-starting-elo))
       (title . ,title)
       (fights . ,plist-fights)
       (num-fights . ,plist-numfights))))
 
 
-(defun org-elo-compare-tabulated (item1 item2)
+(defun elosort-compare-tabulated (item1 item2)
   "Compare tabulated items by rating"
   (let ((elo1
          (pcase
@@ -68,50 +68,50 @@
             (let-alist tabulated-id .elo)))))
     (< elo1 elo2)))
 
-(defvar org-elo-list-top-mode-map
+(defvar elosort-list-top-mode-map
   (let ((map (make-sparse-keymap)))
-    (define-key map (kbd "f") #'org-elo-fight-from-top)
+    (define-key map (kbd "f") #'elosort-fight-from-top)
     map)
-  "Org-elo scoreboard mode keymap.")
+  "elosort scoreboard mode keymap.")
 
-(define-derived-mode org-elo-list-top-mode tabulated-list-mode "org-elo-list-top-mode"
+(define-derived-mode elosort-list-top-mode tabulated-list-mode "elosort-list-top-mode"
   "Elo Top"
   (setq truncate-lines t)
   (setq buffer-read-only t)
-  (setq tabulated-list-format [("rating" 8 org-elo-compare-tabulated)
+  (setq tabulated-list-format [("rating" 8 elosort-compare-tabulated)
 			       ("title" 50 t)
                                ("num_games" 16 t)])
   (setq tabulated-list-sort-key
         (cons "rating" :inverted))
-  (add-hook 'tabulated-list-revert-hook #'org-elo-list-top-refresh nil t))
+  (add-hook 'tabulated-list-revert-hook #'elosort-list-top-refresh nil t))
 
-(put 'org-elo-list-top-mode 'mode-class 'special)
+(put 'elosort-list-top-mode 'mode-class 'special)
 
-(defun org-elo-list-top (filename)
+(defun elosort-list-top (filename)
   "Display the score board."
   (interactive "fSelect file: ")
   (let ((elo-buf
          (get-buffer-create
            "*Org Elo Top*")))
     (switch-to-buffer elo-buf)
-    (org-elo-list-top-mode)
+    (elosort-list-top-mode)
     (setq-local elo-source-buffer elo-buf)
-    (setq org-elo-file filename)
-    (org-elo-list-top-refresh)))
+    (setq elosort-file filename)
+    (elosort-list-top-refresh)))
 
-(defun org-elo-list-top-refresh ()
+(defun elosort-list-top-refresh ()
   "Refresh elo top."
   (interactive)
   (let* ((entries
           (save-excursion
-            (with-current-buffer (find-file-noselect org-elo-file)
+            (with-current-buffer (find-file-noselect elosort-file)
               (org-map-entries
-               (lambda () (org-elo-tabulate (org-elo-get-alist))))))))
+               (lambda () (elosort-tabulate (elosort-get-alist))))))))
     (setq tabulated-list-entries entries)
     (tabulated-list-init-header)
     (tabulated-list-print)))
 
-(defun org-elo-tabulate (alist)
+(defun elosort-tabulate (alist)
   "Item alist as tabulated-list entry."
   (let-alist alist
     (list
@@ -121,35 +121,35 @@
       (list .title)
       (list (number-to-string .num-fights))))))
 
-(defun org-elo-list-top-from-fight ()
-  "Call from fight mode providing `org-elo-file' as argument"
+(defun elosort-list-top-from-fight ()
+  "Call from fight mode providing `elosort-file' as argument"
   (interactive)
-  (org-elo-list-top org-elo-file))
+  (elosort-list-top elosort-file))
 
 
-(defvar org-elo-fight-mode-map
+(defvar elosort-fight-mode-map
   (let ((map (make-sparse-keymap)))
-    (define-key map (kbd ",") #'org-elo-fight-win1)
-    (define-key map (kbd ".") #'org-elo-fight-win2)
-    (define-key map (kbd "g") #'org-elo-fight-revert)
-    (define-key map (kbd "t") #'org-elo-list-top-from-fight)
+    (define-key map (kbd ",") #'elosort-fight-win1)
+    (define-key map (kbd ".") #'elosort-fight-win2)
+    (define-key map (kbd "g") #'elosort-fight-revert)
+    (define-key map (kbd "t") #'elosort-list-top-from-fight)
     map)
   "Fight mode keymap.")
 
-(defun org-elo-fight-win1 ()
+(defun elosort-fight-win1 ()
   "Set p1 as winner and update"
   (interactive)
-  (org-elo-fight-update t))
+  (elosort-fight-update t))
 
-(defun org-elo-fight-win2 ()
+(defun elosort-fight-win2 ()
   "Set p2 as winner and update"
   (interactive)
-  (org-elo-fight-update nil))
+  (elosort-fight-update nil))
 
-(defun org-elo-fight-update (p1-winner-p)
+(defun elosort-fight-update (p1-winner-p)
   "Update ratings DB for current pair."
-  (let* ((p1 (copy-alist org-elo-p1))
-         (p2 (copy-alist org-elo-p2))
+  (let* ((p1 (copy-alist elosort-p1))
+         (p2 (copy-alist elosort-p2))
          (p1-title (let-alist p1 .title))
          (p2-title (let-alist p2 .title))
          (p1-fights (let-alist p1 .fights))
@@ -164,13 +164,13 @@
            (or
             (plist-get p1-fights p2-title #'equal)
             "0"))))
-         (p1-elo (or (let-alist p1 .elo) org-elo-starting-elo))
-         (p2-elo (or (let-alist p2 .elo) org-elo-starting-elo))
+         (p1-elo (or (let-alist p1 .elo) elosort-starting-elo))
+         (p2-elo (or (let-alist p2 .elo) elosort-starting-elo))
          (new-elos
           (if
               p1-winner-p
-              (org-elo-compute-elo p1-elo p2-elo)
-            (org-elo-compute-elo p2-elo p1-elo)))
+              (elosort-compute-elo p1-elo p2-elo)
+            (elosort-compute-elo p2-elo p1-elo)))
          (p1-new-elo (if p1-winner-p (car new-elos) (cdr new-elos)))
          (p2-new-elo (if p1-winner-p (cdr new-elos) (car new-elos)))
          (p1-new-fights (plist-put p1-fights p2-title
@@ -178,30 +178,30 @@
          (p2-new-fights (plist-put p2-fights p1-title
                                (number-to-string p1-vs-p2-num) #'equal)))
     (save-excursion
-      (with-current-buffer (find-file-noselect org-elo-file)
-        (org-entry-put (org-elo-get-by-title p1-title) "ELO" (number-to-string p1-new-elo))
-        (org-entry-put (org-elo-get-by-title p2-title) "ELO" (number-to-string p2-new-elo))
-        (org-entry-delete (org-elo-get-by-title p1-title) "ELO_FIGHTS")
-        (org-entry-delete (org-elo-get-by-title p2-title) "ELO_FIGHTS")
+      (with-current-buffer (find-file-noselect elosort-file)
+        (org-entry-put (elosort-get-by-title p1-title) "ELO" (number-to-string p1-new-elo))
+        (org-entry-put (elosort-get-by-title p2-title) "ELO" (number-to-string p2-new-elo))
+        (org-entry-delete (elosort-get-by-title p1-title) "ELO_FIGHTS")
+        (org-entry-delete (elosort-get-by-title p2-title) "ELO_FIGHTS")
         (apply `(org-entry-put-multivalued-property
-                 ,(org-elo-get-by-title p1-title)
+                 ,(elosort-get-by-title p1-title)
                  "ELO_FIGHTS"
                  ,@p1-new-fights))
         (apply `(org-entry-put-multivalued-property
-                 ,(org-elo-get-by-title p2-title)
+                 ,(elosort-get-by-title p2-title)
                  "ELO_FIGHTS"
                  ,@p2-new-fights))
         (save-buffer)))
-  (org-elo-fight-revert)))
+  (elosort-fight-revert)))
 
-(define-derived-mode org-elo-fight-mode special-mode "org-elo-fight-mode"
+(define-derived-mode elosort-fight-mode special-mode "elosort-fight-mode"
   "Fight!"
   (setq buffer-read-only t)
-  (defvar-local org-elo-buf nil)
-  (defvar-local org-elo-p1 nil "Candidate #1")
-  (defvar-local org-elo-p2 nil "Candidate #2"))
+  (defvar-local elosort-buf nil)
+  (defvar-local elosort-p1 nil "Candidate #1")
+  (defvar-local elosort-p2 nil "Candidate #2"))
 
-(put 'org-elo-fight-mode 'mode-class 'special)
+(put 'elosort-fight-mode 'mode-class 'special)
 
 ;;;
 (defun swap (LIST el1 el2)
@@ -219,7 +219,7 @@ shuffling is done in place."
              (swap LIST i j)))
   LIST)
 
-(defun org-elo-next-pair (items)
+(defun elosort-next-pair (items)
   "Return (cons item1 item2) from items list."
   (let* ((entries (copy-sequence items))
          (sorted
@@ -242,31 +242,31 @@ shuffling is done in place."
                         (< afs bfs)))))))
     (cons p1 p2)))
 
-(defun org-elo-fight-revert ()
+(defun elosort-fight-revert ()
   "Refresh buffer, get next pair."
   (interactive)
   (save-excursion
-    (with-current-buffer (find-file-noselect org-elo-file)
-      (setq entries (shuffle (org-map-entries #'org-elo-get-alist)))))
+    (with-current-buffer (find-file-noselect elosort-file)
+      (setq entries (shuffle (org-map-entries #'elosort-get-alist)))))
   (let* ((inhibit-read-only t)
-         (pair (org-elo-next-pair entries))
+         (pair (elosort-next-pair entries))
          (p1 (car pair))
          (p1-elo (let-alist p1 .elo))
          (p1-title (let-alist p1 .title))
          (p2 (cdr pair))
          (p2-elo (let-alist p2 .elo))
          (p2-title (let-alist p2 .title)))
-    (setq-local org-elo-p1 p1)
-    (setq-local org-elo-p2 p2)
+    (setq-local elosort-p1 p1)
+    (setq-local elosort-p2 p2)
     (delete-region (point-min) (point-max))
-    (insert-button p1-title 'action (lambda (_) (org-elo-fight-win1)))
+    (insert-button p1-title 'action (lambda (_) (elosort-fight-win1)))
     (insert " vs ")
-    (insert-button p2-title 'action (lambda (_) (org-elo-fight-win2)))
+    (insert-button p2-title 'action (lambda (_) (elosort-fight-win2)))
     (insert " ")))
 
 
 
-(defun org-elo-fight (filename)
+(defun elosort-fight (filename)
   "Start competition."
   (interactive "fSelect file: ")
   (let ((fight-buf
@@ -275,36 +275,36 @@ shuffling is done in place."
     (pop-to-buffer-same-window fight-buf)
     (with-current-buffer fight-buf
       (setq inhibit-read-only t)
-      (org-elo-fight-mode)
-      (setq-local org-elo-buf fight-buf)
-      (setq org-elo-file filename)
-      (org-elo-fight-revert))))
+      (elosort-fight-mode)
+      (setq-local elosort-buf fight-buf)
+      (setq elosort-file filename)
+      (elosort-fight-revert))))
 
-(defun org-elo-fight-from-top ()
+(defun elosort-fight-from-top ()
   "Call from fight mode providing filename"
   (interactive)
-  (org-elo-fight org-elo-file))
+  (elosort-fight elosort-file))
 
-(defun org-elo-fight-revert ()
+(defun elosort-fight-revert ()
   "Refresh buffer, get next candidates."
   (interactive)
   (save-excursion
-    (with-current-buffer (find-file-noselect org-elo-file)
-      (setq entries (shuffle (org-map-entries #'org-elo-get-alist)))))
+    (with-current-buffer (find-file-noselect elosort-file)
+      (setq entries (shuffle (org-map-entries #'elosort-get-alist)))))
   (let* ((inhibit-read-only t)
-         (pair (org-elo-next-pair entries))
+         (pair (elosort-next-pair entries))
          (p1 (car pair))
          (p1-elo (let-alist p1 .elo))
          (p1-title (let-alist p1 .title))
          (p2 (cdr pair))
          (p2-elo (let-alist p2 .elo))
          (p2-title (let-alist p2 .title)))
-    (setq-local org-elo-p1 p1)
-    (setq-local org-elo-p2 p2)
+    (setq-local elosort-p1 p1)
+    (setq-local elosort-p2 p2)
     (delete-region (point-min) (point-max))
-    (insert-button p1-title 'action (lambda (_) (org-elo-fight-win1)))
+    (insert-button p1-title 'action (lambda (_) (elosort-fight-win1)))
     (insert " vs ")
-    (insert-button p2-title 'action (lambda (_) (org-elo-fight-win2)))
+    (insert-button p2-title 'action (lambda (_) (elosort-fight-win2)))
     (insert " ")))
 
-(provide 'org-elo)
+(provide 'elosort)
